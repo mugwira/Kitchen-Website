bb71e13d901a87b486c1dbb224e444783dcad5aa
Mugwira <mugwiram@gmail.com>
2021-09-28 11:08:57 +0200

    Modified App component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App ">
         <Header />
         <Switch>
           <Route exact path="/products">
